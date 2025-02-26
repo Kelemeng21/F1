a68173381a077c97ade3e9d143b2f8873de37e87
Kelemeng21 <gaborkelemen21@gmail.com>
2025-02-26 10:38:41 +0100

    Add project files.

---
+++ F1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Web;
+using System.Globalization;
+
+namespace F1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "C:\\Users\\Ny20Kelemeng\\source\\repos\\F1\\F1\\src\\melbourne2009.txt";
+
+            List<adatok> verseny = new List<adatok>();
+
+            using (StreamReader sr = new StreamReader(file))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] parts = line.Split(';');
+                    string name = parts[0];
+                    string country = parts[1];
+                    string team = parts[2];
+                    if (!int.TryParse(parts[3], out int position))
+                    {
+                        continue;
+                    }
+                    if (!double.TryParse(parts[4], NumberStyles.Float, CultureInfo.InvariantCulture, out double sec))
+                    {
+                        continue;
+                    }
+                    if (!int.TryParse(parts[5], out int points))
+                    {
+                        continue;
+                    }
+
+                    verseny.Add(new adatok(name, country, team, position, sec, points));
+                }
+            }
+
+            Console.WriteLine($"1. feladat: ennyi pilóta ért a célba {verseny.Count()}");
+
+            var f2 = verseny.Sum(x => x.points);
+
+            Console.WriteLine($"2. feladat: ennyi pont került kiosztásra {f2}");
+
+            var f3 = verseny.Where(x => x.country == "germany");
+
+            Console.WriteLine($"3. feladat: Ennyi német versenyző van {f3.Count()}");
+
+            var f4 = verseny.Where(x => x.points > 0).Select(x => x.team).Distinct().ToList();
+            Console.WriteLine("4. feladat azok a csapatok akik pontot szereztek");
+            foreach (var item in f4)
+            {
+                Console.WriteLine($"\t{item} ");
+            }
+
+            var f5 = verseny.Min(x => x.sec);
+
+            foreach (var item in verseny)
+            {
+                if (item.sec == f5)
+                {
+                    Console.WriteLine($"A versenyző neve {item.name}");
+                    Console.WriteLine($"Az ideje 01:34:{item.sec}");
+                }
+            }
+        }
+    }
+}
---
+++ F1/adatok.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace F1
+{
+    internal class adatok
+    {
+        public string name { get; set; }
+        public string country { get; set; }
+        public string team { get; set; }
+        public int position { get; set; }
+        public double sec { get; set; }
+        public int points { get; set; }
+
+        public adatok(string name, string country, string team, int position, double sec, int points)
+        {
+            this.name = name;
+            this.country = country;
+            this.team = team;
+            this.position = position;
+            this.sec = sec;
+            this.points = points;
+        }
+
+        public override string ToString()
+        {
+            return $"name: {name}, country: {country}, team: {team}, " +
+                   $"position: {position}, sec: {sec}, points: {points}";
+        }
+    }
+}
